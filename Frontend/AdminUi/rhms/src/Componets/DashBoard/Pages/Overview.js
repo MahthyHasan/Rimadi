c5ee44fa7225de13721cbdf200bf82aafdfcde22
HasanMJMM <maheng907@gmail.com>
2024-07-22 18:36:57 +0530

    updating project structure.

---
+++ Frontend/AdminUi/rhms/src/Componets/DashBoard/Pages/Overview.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Layout from '../../../Layout/Layout';
+
+function Overview() {
+  return (
+    <>
+    <Layout>
+        
+    </Layout>
+    </>
+  )
+}
+
+export default Overview
