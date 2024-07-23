d1faba59e2c752ed9479ddd8cc08bb50deb50145
HasanMJMM <maheng907@gmail.com>
2024-07-23 20:16:09 +0530

    Start Working on DAshboard Overview Page

--- Frontend/AdminUi/rhms/src/Componets/DashBoard/Pages/Overview.js
+++ Frontend/AdminUi/rhms/src/Componets/DashBoard/Pages/Overview.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import "./dashPage.css";
 import Layout from '../../../Layout/Layout';
+import TopNavigationBar from '../Components/TopNavigationBar';
+
+
+
+const tabs = [
+  { label: 'Overview', link: '/dashboard-overview' },
+  { label: 'Expenses', link: '/dashboard-expenses' },
+  { label: 'Bill', link: '/dashboard-bill' },
+  { label: 'Smart Key', link: '/dashboard-smartKey' },
+];
 
 function Overview() {
   return (
     <>
     <Layout>
-        
+      <div className='container'>
+        <div className='top-Navigation-bar'>
+          <TopNavigationBar tabs={tabs}/>
+        </div>
+      </div>        
     </Layout>
     </>
   )
