ab23eeb96ad94c106d810fffac0827c8cb1d9858
HasanMJMM <maheng907@gmail.com>
2024-07-30 12:38:30 +0530

    iitiating Expencess Page

--- Frontend/AdminUi/rhms/src/App.js
+++ Frontend/AdminUi/rhms/src/App.js
@@ -2,13 +2,15 @@ import React from "react";
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Overview from "./Componets/DashBoard/Pages/Overview";
+import Expenses from "./Componets/DashBoard/Pages/Expenses";
 
 
 function App() {
   return (
     <div>
       <Routes>
-         <Route path="/dashboard-overview" element={<Overview />} />        
+         <Route path="/dashboard-overview" element={<Overview />} />
+         <Route path="/dashboard-expenses" element={<Expenses />} />            
       </Routes>
     </div>
   );
---
+++ Frontend/AdminUi/rhms/src/Componets/DashBoard/Pages/Expenses.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./dashPage.css";
+import Layout from "../../../Layout/Layout";
+import TopNavigationBar from "../Components/TopNavigationBar";
+
+function Expenses() {
+  return (
+   <Layout>
+    
+   </Layout>
+  )
+}
+
+export default Expenses
