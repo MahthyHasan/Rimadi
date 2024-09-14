902b15c57d9e877f7e19383f23b7befcdd299b21
HasanMJMM <maheng907@gmail.com>
2024-09-15 04:19:35 +0530

    updating Manage Booking Part

--- Frontend/AdminUi/rhms/src/Componets/ManageRoom/Pages/Properties.js
+++ Frontend/AdminUi/rhms/src/Componets/ManageRoom/Pages/Properties.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React from "react";
 import ReactDOM from "react-dom";
-import TopNavigationBar from '../Components/TopNavigationBar';
-import Layout from '../../../Layout/Layout';
+import TopNavigationBar from "../Components/TopNavigationBar";
+import Layout from "../../../Layout/Layout";
 import "../../DashBoard/Pages/dashPage.css";
-import TopCardsSection from '../Components/TopCardsSection';
+import "./mrPage.css";
+import TopCardsSection from "../Components/TopCardsSection";
+import FilterIcon from "../../../assets/DashBoard/filter.svg";
 
 const tabs = [
 	{ label: "Properties", link: "/manageR/properties" },
@@ -13,22 +15,36 @@ const tabs = [
 ];
 
 function Properties() {
-  return (
-    <Layout >
-        <div className='container'>
-            <div className='top-Navigation-bar'>
-                <TopNavigationBar tabs={tabs} />
-            </div>
-            <div className="top-info-cards">
+	return (
+		<Layout>
+			<div className="container">
+				<div className="top-Navigation-bar">
+					<TopNavigationBar tabs={tabs} />
+				</div>
+				<div className="top-info-cards">
 					<TopCardsSection />
+				</div>
+				<div className="mt-4 properties-space-for-cards-mr">
+					<div className="d-flex flex-row mt-2">
+						<div className="p-2">
+							<button className="Filter-button-on-expense-table">
+								<img className="filter-icon-inside-a-button" src={FilterIcon} />
+								<h6 className="filter-text-inside-a-button mt-2">Filter</h6>
+							</button>
+						</div>
+						<div className="p-2">
+							<input
+								type="text"
+								className="p-2 expense-table-input-text-field-mr"
+								placeholder="Search Content"
+							/>
+						</div>
+					</div>
+          <div class="d-flex justify-content-center">...</div>
+				</div>
 			</div>
-            <div className='row mt-2 properties-space-for-cards'>
-                
-            </div>
-        </div>
-
-    </Layout>
-  )
+		</Layout>
+	);
 }
 
-export default Properties
+export default Properties;
