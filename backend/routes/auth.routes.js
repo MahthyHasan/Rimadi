8de1d323f990aab66d77f39a02c5fb36a688de02
Mahthy Hasan <mahthyhasan01@gmail.com>
2025-01-16 18:38:04 +0530

    2- Backend Auth Functions have been completed

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,12 +1,12 @@
 import express from 'express';
-import { logIn, logOut, signUp } from '../controllers/auth.controller.js';
+import { getUser, logIn, logOut, signUp } from '../controllers/auth.controller.js';
+import { protectedRoute } from '../midlelayers/protectRoute.js';
 
 const router = express.Router();
 
+router.get('/getUser', protectedRoute, getUser);
 router.post('/signUp', signUp );
-
 router.post('/logIn', logIn);
-
 router.post('/logOut', logOut);
 
 export default router;
